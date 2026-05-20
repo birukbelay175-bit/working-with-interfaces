cc493aced3f21692b0942057032a5d54bdabd449
birukbelaytafese <birukbelay175@gmail.com>
2026-05-20 06:09:31 -0500

    first commit

---
+++ src/main/java/com/pluralsight/Dog.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Dog {
+
+    private String name;
+    private String breed;
+
+    public Dog(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+}
